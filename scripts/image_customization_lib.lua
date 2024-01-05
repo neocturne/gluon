fa09986e54a6a8730c0003febd7bc6fc87783b96
Matthias Schiffer <mschiffer@universe-factory.net>
2024-01-05 11:40:11 +0100

    scripts: image_customization_lib: add include() function
    Add a simple way to include image customization Lua snippets.
    Can be used to split long files, to include the same options multiple
    times in different conditional branches, or even to pass values back to
    the including file using return.
    
    Relative paths are interpreted relative to the site root (where
    image-customization.lua is located). Relative includes would become
    confusing when subdirectories are involved (as they are still interpreted
    relative to the site root, and proper tracking of current directory for
    each include seems fairly complex for a very niche use case), so we simply
    reject this case for now; this way, we can implement this however we
    want in the future if deemed necessary, without a breaking change.

--- scripts/image_customization_lib.lua
+++ scripts/image_customization_lib.lua
@@ -79,6 +79,18 @@ local function evaluate_device(env, dev)
 		return dev.options.class == class
 	end
 
+	function funcs.include(path)
+		if string.sub(path, 1, 1) ~= '/' then
+			assert(
+				string.find(path, '/') == nil,
+				'incorrect use of include(): including files from subdirectories is unsupported')
+			path = env.GLUON_SITEDIR .. '/' .. path
+		end
+		local f = assert(loadfile(path))
+		setfenv(f, funcs)
+		return f()
+	end
+
 	-- Evaluate the feature definition files
 	setfenv(M.customization_file, funcs)
 	M.customization_file()
